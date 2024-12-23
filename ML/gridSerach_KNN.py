9379a4cc18f4e5070d043ec02130b6192d5513bf
Lucas Bueno <53531008+LucasBusavs@users.noreply.github.com>
2024-12-23 15:03:50 -0300

    Update dataset

--- ML/gridSerach_KNN.py
+++ ML/gridSerach_KNN.py
@@ -1,22 +1,32 @@
+import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import StandardScaler
 
-# Carregar o dataset (substitua pelo seu dataset)
-data = load_iris()
-X = data.data
-y = data.target
+# data = load_iris()
+# X = data.data
+# y = data.target
+
+dataset = pd.read_csv('docs/db/dados_preprocessados.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 
 # Dividir os dados em treino e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+# Feature Scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
 # Definir o modelo KNN
 knn = KNeighborsClassifier()
 
 # Definir os hiperparâmetros para otimização
 param_grid = {
-    'n_neighbors':  list(range(1, 102)),  # Número de vizinhos
+    'n_neighbors':  list(range(8, 102)),  # Número de vizinhos
     'weights': ['uniform', 'distance'],  # Peso das amostras
     'metric': ['euclidean', 'manhattan', 'minkowski']  # Métricas de distância
 }
@@ -42,7 +52,3 @@ best_knn = grid_search.best_estimator_
 y_pred = best_knn.predict(X_test)
 print("\nRelatório de classificação:")
 print(classification_report(y_test, y_pred))
-
-# Exibir a acurácia no conjunto de teste
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Acurácia no conjunto de teste: {accuracy:.2f}")
