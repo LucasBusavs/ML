bde370ac46d20edcb97adf7fc12adfcf3b4c162b
Lucas Bueno <53531008+LucasBusavs@users.noreply.github.com>
2025-04-13 20:04:14 -0300

    score comments

--- ML/score.py
+++ ML/score.py
@@ -2,24 +2,49 @@ from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 from tabulate import tabulate
 
 
-def pipeline_score(*args, weights=None, verbose=False):
+def pipeline_score(*args, weights=None, verbosity=False):
     """
-    Função para calcular um score personalizado para modelos de ML.
+    Function to calculate a custom score for ML models.
+    The score is a weighted sum of accuracy, precision, recall, and F-score.
+    The weights for each metric can be customized using the 'weights' parameter.
 
-    Pode ser chamada de duas formas:
-    1. pipeline_score(y_true, y_pred, weights=None, verbose=False)  -> Para avaliações manuais
-    2. pipeline_score(estimator, X, y, weights=None, verbose=False) -> Para GridSearchCV e RandomSearchCV
+    The default weights are:
+    - accuracy: 1
+    - precision: 1
+    - recall: 1
+    - f_score: 1
 
+    Can be called in two ways:
+    1. pipeline_score(y_true, y_pred)  -> For manual evaluations
+    2. pipeline_score(estimator, X, y) -> For GridSearchCV and RandomSearchCV
+
+    Parameters
+    ----------
+    args : tuple
+        - (y_true, y_pred): True labels and predicted labels.
+        - (estimator, X, y): Estimator, feature set, and true labels.
+    weights : dict, optional
+        Custom weights for accuracy, precision, recall, and F-score.
+    verbosity : bool, optional
+        If True, prints the detailed metrics for each class.
+
+    Returns
+    -------
+    score: float
+        The calculated score based on the provided weights and metrics.
     """
-    # GridSearchCV e RandomSearchCV
+
+    # GridSearchCV & RandomSearchCV
     if len(args) == 3:
-        estimator, X, y_true = args  # Extraindo os parâmetros
-        y_pred = estimator.predict(X)  # Fazendo a predição
+        estimator, X, y_true = args
+        y_pred = estimator.predict(X)
     elif len(args) == 2:
-        y_true, y_pred = args  # Caso padrão: y_true e y_pred já fornecidos
+        y_true, y_pred = args
     else:
         raise ValueError(
-            "Número de argumentos inválido. Esperado (y_true, y_pred) ou (estimator, X, y)")
+            "Invalid number of arguments. Expected 2 or 3 arguments."
+            " (y_true, y_pred) or (estimator, X, y_true)."
+        )
 
     default_weights = {
         'accuracy': 1,
@@ -36,20 +61,21 @@ def pipeline_score(*args, weights=None, verbose=False):
         elif (default_weights['precision'] < default_weights['recall']):
             beta = 0.5
 
-    # Calcula as métricas
+    # Calculate metrics
     accuracy = accuracy_score(y_true, y_pred)
     precision, recall, fscore, support = precision_recall_fscore_support(
-        y_true, y_pred, average=None, zero_division=0)
+        y_true, y_pred, beta=beta, average=None, zero_division=0
+    )
 
-    if verbose:
+    if verbosity:
         print(f"Accuracy: {accuracy:.2f}\n")
 
         table_data = [
-            [f"Classe {i}", f"{precision[i]:.2f}",
+            [f"Class {i}", f"{precision[i]:.2f}",
                 f"{recall[i]:.2f}", f"{fscore[i]:.2f}", support[i]]
             for i in range(len(precision))
         ]
-        headers = ["Classe", "Precision", "Recall", "F-score", "Support"]
+        headers = ["Class", "Precision", "Recall", "F-score", "Support"]
         print(tabulate(table_data, headers=headers, tablefmt="grid"))
 
     score = default_weights['accuracy'] * accuracy
@@ -57,4 +83,4 @@ def pipeline_score(*args, weights=None, verbose=False):
         score += default_weights['precision'] * precision[i]
         score += default_weights['recall'] * recall[i]
         score += default_weights['f_score'] * fscore[i]
-    return score
+    return float(score)
